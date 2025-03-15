37e674929446b8445fbfe90686c8a9c12012a23c
1Brandao <guilhermebrandao789@gmail.com>
2025-03-15 14:03:33 -0300

    Controller add - v1.0

--- src/main/java/com/brandao/atividades_aula_02/AtividadeAula02Controller.java
+++ src/main/java/com/brandao/atividades_aula_02/AtividadeAula02Controller.java
@@ -1,4 +1,16 @@
 package com.brandao.atividades_aula_02;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class AtividadeAula02Controller {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
 }
